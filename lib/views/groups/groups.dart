37488f67fcd803d343a6bbeb9c2646b637b9ece6
sushant102004 <sushant.dhiman9812@gmail.com>
2022-11-21 14:24:01 +0530

    Simple Group Card Implemented

--- lib/views/groups/groups.dart
+++ lib/views/groups/groups.dart
@@ -11,7 +11,7 @@ class Groups extends StatefulWidget {
 
 class _GroupsState extends State<Groups> {
   final constants = Get.put(Constants());
-  bool hasGroupCreated = false;
+  bool hasGroupCreated = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +25,51 @@ class _GroupsState extends State<Groups> {
         ),
       ),
       body: hasGroupCreated
-          ? Container()
+          ? Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(Get.width / 20),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: Get.height / 11,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade100,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade300,
+                            offset: const Offset(0.0, 0.0),
+                            blurRadius: 5.0,
+                            spreadRadius: 2.0,
+                          )
+                        ]),
+                    child: ListTile(
+                      title: Text(
+                        'Group Title',
+                        style: TextStyle(
+                            color: Colors.grey.shade800,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17),
+                      ),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: constants.primaryColor,
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_forward,
+                        ),
+                        onPressed: () {},
+                      ),
+                      subtitle: const Text(
+                        'Sushant, Kunal, Vishal....',
+                        maxLines: 1,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           : Padding(
               padding: EdgeInsets.all(Get.width / 20),
               child: Center(
